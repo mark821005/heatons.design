7321cdc850fb1ad5db9bc28bbc7c2b41063c7ef9
Mark Adams <mark.adams82@gmail.com>
2025-12-25 22:47:19 +0000

    Adds page transition to About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router";
+import { AnimatedPage } from "@/components/AnimatedPage";
+import Header from "@/components/ui/Header";
+import Footer from "@/components/ui/Footer";
 
 export default function About() {
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold">About Page</h1>
-      <Link to="/" className="text-blue-500 underline mt-4 block">
-        Back to Home
-      </Link>
-    </div>
+    <AnimatedPage>
+      <Header />
+      <div className="p-10">
+        <h1 className="text-3xl font-bold">About Page</h1>
+        <Link to="/" className="text-blue-500 underline mt-4 block">
+          Back to Home
+        </Link>
+      </div>
+      <Footer />
+    </AnimatedPage>
   );
 }
