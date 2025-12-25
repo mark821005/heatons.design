70e694ffabce0165dc395e2e8014f5a943e6e75e
Mark Adams <mark.adams82@gmail.com>
2025-12-25 22:48:40 +0000

    Hero component

---
+++ src/components/ui/Hero.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+import { MotionCarousel } from "@/components/ui/MotionCarousel";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="md:flex md:flex-row md:justify-start md:items-center mb-24 min-h-[400px]">
+        <div className="basis-3/8">
+          <h1 className="scroll-m-20 text-5xl">your life, reimagined.</h1>
+        </div>
+        <div className="basis-5/8">
+          <MotionCarousel />
+        </div>
+      </div>
+    </>
+  );
+}
