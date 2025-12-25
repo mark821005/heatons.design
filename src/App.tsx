b7070a7a2bd9a4756d152bbd73df2f3934d4c455
Mark Adams <mark.adams82@gmail.com>
2025-12-25 22:45:47 +0000

    Add browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route, useLocation } from "react-router";
+import { AnimatePresence } from "framer-motion";
+import { ScrollProgress } from "./components/ui/ScrollProgress";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 function App() {
+  const location = useLocation();
   return (
-    <>
+    <AnimatePresence mode="wait">
       <div className="container mx-auto px-4">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+      <div className="fixed right-0 bottom-0 left-0 z-50 w-full">
+        <ScrollProgress />
+      </div>
+    </AnimatePresence>
   );
 }
 
