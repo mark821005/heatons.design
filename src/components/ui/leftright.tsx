7127078abf74e153dbc5819830793c9c8747ee54
Mark Adams <mark.adams82@gmail.com>
2025-12-26 21:43:34 +0000

    Adds text colour scroll animation

--- src/components/ui/leftright.tsx
+++ src/components/ui/leftright.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Leftright({ heading, content }: Props) {
   return (
     <>
-      <div className="md:flex md:flex-row md:justify-start md:items-start my-24">
+      <div className="md:flex md:flex-row md:justify-start md:items-start my-24 scrolling-text">
         <div className="basis-1/2">
           <h1 className="scroll-m-20 text-4xl">{heading}</h1>
         </div>
