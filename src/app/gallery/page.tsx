60fabb1277f6d83840c9089bf197c42d1fc6b536
Mark Adams <mark.adams82@gmail.com>
2026-01-18 14:12:40 +0000

    Gallery text content

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -4,33 +4,43 @@ import { AnimatedPage } from "@/components/AnimatedPage";
 import Leftright from "@/components/ui/leftright";
 import { Gallery } from "@/components/Gallery";
 import { galleryData } from "@/data/gallery";
+import { Button } from "@/components/ui/button";
+import { ArrowUpRight } from "lucide-react";
 
 export default function About() {
   return (
     <AnimatedPage>
       <main>
         <Leftright
-          heading={"welcome to heatons.design"}
+          heading={"artwork examples gallery"}
           content={
             <div className="scrolling-text">
               <p>
-                Once upon a time, in a far-off land, there was a very lazy king
-                who spent all day lounging on his throne. One day, his advisors
-                came to him with a problem: the kingdom was running out of
-                money.
+                Get inspired by browsing the gallery below. Keep in mind things
+                like composition, colour, clothing, art style when ordering in
+                order to get a result that you really love and represents you. I
+                always aim to get as close to the style you want as possible!
               </p>
               <p>
-                Once upon a time, in a far-off land, there was a very lazy king
-                who spent all day lounging on his throne. One day, his advisors
-                came to him with a problem: the kingdom was running out of
-                money.
+                Click any image to view a larger version and make a note of any
+                of the HD reference numbers that stand out to you or that you
+                want to use as an influence on your piece. When you're ready,
+                drop me an email at{" "}
+                <a
+                  href="mailto:heatons.design@gmail.com"
+                  title="Email Heatons Design"
+                >
+                  heatons.design@gmail.com
+                </a>{" "}
+                with either your full brief or the idea you've got. Often its
+                best for us to speak directly to get your requirements nailed
+                down prior to work starting.
               </p>
-              <p>
-                Once upon a time, in a far-off land, there was a very lazy king
-                who spent all day lounging on his throne. One day, his advisors
-                came to him with a problem: the kingdom was running out of
-                money.
-              </p>
+              <a href="/contact" title="Contact Heatons Design">
+                <Button size="sm" className="mt-6 mr-6">
+                  Contact <ArrowUpRight />
+                </Button>
+              </a>
             </div>
           }
         />
